f6afb31221cb365ff3e11dd503506628f7527f33
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-09 17:00:57 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@
 ActiveRecord::Schema.define(version: 20190305033944) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string  "username"
+    t.string  "password_digest"
+    t.decimal "balance"
   end
 
 end
