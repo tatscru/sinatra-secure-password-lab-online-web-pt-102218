86172515fa83b4794c76ac34120ba44bf568fd7e
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-09 17:07:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-      user= User.find_by(username: params[:username]) 
+    user= User.find_by(username: params[:username]) 
     if user && user.authenticate(password: params[:password])
       session[:user_id] = user.id 
       erb "/account"
